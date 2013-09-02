e0dffa2db5db1548e98e5f6062953fb092e94071
xsuii <sherlock0he@gmail.com>
2013-09-02 22:19:01 +0800

    add file download module

--- www/js/chat.js
+++ www/js/chat.js
@@ -1,35 +1,83 @@
 // JavaScript Document
+window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
+var USER = localStorage.username;
+var UID = localStorage.uid;
+var fs = null;
 var wsUriTest = "ws://echo.websocket.org"
 var wsuri = "ws://xsuii.meibu.net:8001/chat"
 var origin = "http://xsuii.meibu.net"
 
 var dbName = "history";
-var tbName = "h" + localStorage.uid;
+var tbName = "h" + UID;
 var db = window.openDatabase(dbName, "1.0", "history store", 1000);
 var f = document.getElementById("file");
 var talks;
 
 function init() {
+	if (window.requestFileSystem) {
+		initFS();
+	}
 	onWebSocket();
+}
+
+//////////////////////////////　File /////////////////////////////
+
+function errorFile(e) {
+	var msg = '';
+	switch (e.code) {
+		case FileError.QUOTA_EXCEEDED_ERR:
+			msg = 'QUOTA_EXCEEDED_ERR';
+			break;
+		case FileError.NOT_FOUND_ERR:
+			msg = 'NOT_FOUND_ERR';
+			break;
+		case FileError.SECURITY_ERR:
+			msg = 'SECURITY_ERR';
+			break;
+		case FileError.INVALID_MODIFICATION_ERR:
+			msg = 'INVALID_MODIFICATION_ERR';
+			break;
+		case FileError.INVALID_STATE_ERR:
+			msg = 'INVALID_STATE_ERR';
+			break;
+	}
+	document.getElementById("chatError").innerHTML = 'Error: ' + msg;
+}
+
+function initFS() {
+	console.log("init filesystem")
+	window.requestFileSystem(window.TEMPORARY, 1024 * 1024, function(filesystem) {
+		fs = filesystem;
+		console.log(fs)
+	}, errorFile);
 }
 
 function upFile() {
 	console.log("file upload fire");
+	var sendTo = document.getElementById("one").value;
+	if (sendTo == null || sendTo == "") {
+		document.getElementById("chatError").innerHTML = "Please fill up the one you send to";
+		return;
+	}
 	var reader = new FileReader();
+	var t = new Date();
 	reader.onloadend = function(bytes) {
 		fName = f.value.substring(f.value.lastIndexOf('\\') + 1);
 		console.log("send file:", fName);
 		doSend(JSON.stringify({
-			"Type": "F",
-			"ToName": "0",
-			"Body": fName
+			"Addressee": sendTo,
+			"Message": fName,
+			"DateTime": t.toUTCString(),
+			"Type": "FILE",
+			"DstT": "S"
 		}));
 		doSend(bytes.target.result);
 	};
 	reader.readAsArrayBuffer(f.files[0]);
 }
 
-// database operate
+////////////////////// database operate  ///////////////////////
+
 function createTable(tx) {
 	console.log("create table", tbName);
 	tx.executeSql('CREATE TABLE IF NOT EXISTS ' + tbName + '(talks)');
@@ -92,6 +140,9 @@ function chatError(err) {
 	para.innerHTML = err;
 }
 
+
+//////////////////////// WebSocket //////////////////////////////
+
 function onWebSocket() {
 	//websocket = new plugins.WebSocket(wsUriTest);
 	websocket = new WebSocket(wsuri);
@@ -111,9 +162,9 @@ function onWebSocket() {
 
 function onOpen(evt) {
 	console.log("CONNECTED");
-	document.getElementById("author").innerHTML = "Author:" + localStorage.username;
-	document.getElementById("uid").innerHTML = "UID:" + localStorage.uid;
-	doSend(localStorage.uid + "+" + localStorage.username); // send uid as identify
+	document.getElementById("author").innerHTML = "Author:" + USER;
+	document.getElementById("uid").innerHTML = "UID:" + UID;
+	doSend(USER + "+" + UID); // send uid as identify
 	//db.transaction(createTable, errorCB, successCB);
 }
 
@@ -122,25 +173,76 @@ function onClose(evt) {
 }
 
 function onMessage(evt) {
-	/*talks = evt.data;
-	console.log("RESPONSE: ", talks);*/
 	talks = JSON.parse(evt.data);
 	console.log("RESPONSE: ", talks);
-	
-	show = "["+talks.DstT+"]"+talks.Author+":"+talks.Message;
 
 	var para = document.getElementById("messageBox");
 	var pre = document.createElement("p");
-	pre.innerHTML = show;
+	pre.innerHTML = "[" + talks.DstT + "]" + talks.Author + ":" + talks.Message;
+	if (talks.Type == "FILE" && talks.Author != "MASTER") {
+		console.log("add file node")
+		pre.style.fontStyle = "italic";
+		pre.style.fontWeight = "bolder";
+		pre.style.color = "#FF0087";
+		pre.style.backgroundColor = "#D5D5D5"
+		pre.style.padding = "5px";
+		pre.setAttribute("author", talks.Author);
+		pre.setAttribute("addressee", talks.Addressee);
+		pre.setAttribute("filename", talks.Message);
+		pre.setAttribute("datetime", talks.DateTime);
+		pre.setAttribute("type", talks.Type);
+		pre.setAttribute("dstt", talks.DstT);
+		pre.onclick = function() { // send download file request
+			console.log(this.getAttribute("filename"));
+			var r = confirm("sure download?  " + this.getAttribute("filename"));
+			if (r == true) {
+				p = {
+					"Author": this.getAttribute("author"),
+					"Addressee": this.getAttribute("addressee"),
+					"Message": this.getAttribute("filename"),
+					"DateTime": this.getAttribute("dateTime"),
+					"Type": this.getAttribute("type"),
+					"DstT": this.getAttribute("dstt")
+				};
+				doSend(JSON.stringify(p))
+			} else {
+				return;
+			}
+		}
+	} else if (talks.Type == "FILE" && talks.Author == "MASTER") { // recieve file
+		file = JSON.parse(talks.Message)
+		console.log(fs,"begin to recieve file",file.FileName);
+		fs.root.getFile(file.FileName, {
+			create: true
+		}, function(fileEntry) {
+			fileEntry.createWriter(function(fileWriter) {
+				console.log("file_writer_test_s");
+				fileWriter.onwriteend = function(e) {
+					
+					console.log("get file done");
+				};
+
+				fileWriter.onerror = function(e) {
+					console.log("write error");
+				};
+
+				var blob = new Blob([file.Body], {type: "text/plain"});
+				fileWriter.write(blob);
+				//fileWriter.write(file.Body);
+			}, errorFile);
+		}, errorFile);
+		console.log("file_writer_test_e");
+		return;
+	}
 	para.appendChild(pre);
 	keepScrollButtom(para);
-
 	db.transaction(addHistory, errorCB, successCB);
 }
 
 function onError(evt) {
 	console.log("ERROR: " + evt.data);
 }
+/////////////////////////////////////////////////////////////////////
 
 function doSend(message) {
 	console.log("SENT: " + message);
@@ -151,34 +253,31 @@ function sendMessage() {
 	var msg = document.getElementById("message").value;
 	var one = document.getElementById("one").value;
 	var group = document.getElementById("group").value;
+	var t = new Date();
+	var pack = {
+		"Message": msg,
+		"DataTime": t.toUTCString(),
+		"Type": "MSG"
+	};
 	if (one) {
 		var para = document.getElementById("messageBox");
 		var pre = document.createElement("p");
-		pre.innerHTML = "[S]" + localStorage.username + ":" + msg;
+		pre.innerHTML = "[S]" + USER + ":" + msg;
 		para.appendChild(pre);
 		keepScrollButtom(para);
 		//msg = "S+" + one + "+" + msg; // single chat
-		msg = JSON.stringify({
-			"Type": "S",
-			"ToName": one,
-			"Body": msg
-		});
+		pack["DstT"] = "S";
+		pack["Addressee"] = one;
 	} else if (group) {
 		//msg = "G+" + group + "+" + msg; // group chat
-		msg = JSON.stringify({
-			"Type": "G",
-			"ToName": group,
-			"Body": msg
-		});
+		pack["DstT"] = "G";
+		pack["Addressee"] = group;
 	} else {
 		//msg = "B+" + msg; // broadcast chat
-		msg = JSON.stringify({
-			"Type": "B",
-			"ToName": "broadcast",
-			"Body": msg
-		});
+		pack["DstT"] = "B";
+		pack["Addressee"] = "broadcast";
 	}
-	doSend(msg);
+	doSend(JSON.stringify(pack));
 }
 
 function keepScrollButtom(node) {
